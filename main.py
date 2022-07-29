49de3b9b1fef35d1befae60e4d220af5594d7f19
unknown <beaufb1999@zoho.com>
2022-07-29 14:40:54 -0500

    New changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.secret_key = "secret"
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_password'] = 'redetpion66'
+app.config['MYSQL_PASSWORD'] = 'redetpion66'
 app.config['MYSQL_DB'] = 'python'
 
 mysql = MySQL(app)
@@ -20,7 +20,7 @@ def login():
     if request.method == 'POST' and 'id' in request.form:
         id = request.form['id']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * from estudiantes WHERE id =%s', (id))
+        cursor.execute('SELECT * from estudiantes WHERE id =%s ', (id,))
         account = cursor.fetchone()
 
         if account:
@@ -28,9 +28,23 @@ def login():
             session['ind'] = account['ind']
             session['id'] = account['id']
 
-            return "Ingreso satisfactoriamente!"
+            return redirect(url_for('home'))
         else:
             msg = 'ID incorrecto!'
 
     
     return render_template("index.html", msg='')
+
+@app.route('/auth/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('ind', None)
+    session.pop('id', None)
+
+    return redirect(url_for('login'))
+
+@app.route('/auth/home')
+def home():
+    if 'loggedin' in session:
+        return render_template("home.html", id=session['id'])
+    return redirect(url_for('login'))
